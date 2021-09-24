4a574d3c07197219a8b2479f5044da0cf379f9f7
SOUMOJIT-CHAKRABORTY <88587765+SOUMOJIT-CHAKRABORTY@users.noreply.github.com>
2021-09-24 20:57:40 +0530

    js creating

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-var iconClick = document.getElementsById("Icon-function")
-var textbox = document.getElementsById("Textbox")
-var button = document.getElementsById("Button")
+//var iconClick = document.getElementsById("Icon-function")
+//var textbox = document.getElementsById("Textbox")
+//var button = document.getElementsById("Button")
+
+var messages = document.getElementById("messages");
+var textbox = document.getElementById("text-input");
+var button1 = document.getElementById("Icon-function");
+var button2 = document.getElementById("button2");
+
+button1.addEventListener("click", function(){
+    var newMessage = document.createElement("p");
+    newMessage.innerHTML = textbox.value;
+    messages.appendChild(newMessage);
+    textbox.value = "";
+})
